9becd957e671c75517bb29d11967d3a276349aec
lrvick <lance@lrvick.net>
2011-04-20 12:58:52 -0400

    update twitter plugin to use Oauth via tweepy

--- Plugins/Twitter.py
+++ Plugins/Twitter.py
@@ -1,23 +1,26 @@
-import twitter
+import tweepy
 
-twit_password = file("Twitter-private","r").readlines()[0].strip()
+#Twitter.cfg must contain:
 
-def postUpdate(user, update):
-    #OLD COMMENT IS OLD - #hardcoded password sucks! but maybe in the future we can accept pm's with ppls passwords and they can post to twitter from here =)
-    # The password is no less hardcoded now, but at least it's in a seperate file.
-    api = twitter.Api(username=user, password=twit_password)
-    if api.PostUpdate(update): return True
+#CONSUMER_KEY=''
+#CONSUMER_SECRET=''
+#ACCESS_KEY=''
+#ACCESS_SECRET=''
 
-def getLatestStatus(user):
-    api = twitter.Api()
-    statuses = api.GetUserTimeline(user)
-    return statuses[0].text
+execfile("Twitter.cfg")
 
+def postUpdate(update):
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    status = api.update_status(update)
+    print status
+    if status: 
+        return True
 
 def on_PRIVMSG(bot, sender, args):
     PREFIX = '!'
     nick, channel, args = sender.split('!', 1)[0], args[0], args[1]
-    
     if args.startswith(PREFIX):
         try: 
             cmd, msg = args.split(' ', 1)  
@@ -25,13 +28,6 @@ def on_PRIVMSG(bot, sender, args):
             cmd, msg = args, ""
         if cmd == '!tweet' and msg:
             try:
-                user = msg.split('<')[0]
-                update = msg.split('<')[1]
-                if postUpdate(user, update): bot.say(channel, "Update posted.")
+                if postUpdate(msg): bot.say(channel, "Update posted.")
             except Exception:
                 print Exception
-            bot.say(channel, getLatestStatus(msg))
-
-
-
-
