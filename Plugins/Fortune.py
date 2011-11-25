61ea5bae2ee53c3f4a7fdf5ec3201e42adafadcf
UdderWeb Team <staff@udderweb.com>
2011-11-25 14:59:10 -0500

    Should stop spamming during down time.

--- Plugins/Fortune.py
+++ Plugins/Fortune.py
@@ -2,6 +2,7 @@ import thread,os
 from time import sleep
 import random
 
+activity = False
 loaded=False
 def on_load(bot):
     global loaded  
@@ -13,19 +14,23 @@ def on_unload(bot):
     loaded=False
 
 def fortuneTimer(bot):
-    global loaded
+    global loaded,activity
     print "FortuneTimer", loaded
     while loaded:
         sleep(random.randrange(10, 60)*60)
-        doFortune(bot, random=True)
+        if activity:
+            doFortune(bot, random=True)
+            activity = False
 
 def on_PRIVMSG(bot,nick,msg):
+    global activity
     source = msg[0]
     if source == bot.nickname:
         source = nick.split("!")[0]
     if msg[1]=="!fortune": #bad , quick 
         doFortune(bot)
-    
+    activity = True
+
 def doFortune(bot, random=False):
     p = os.popen("fortune -s")
     fortune = p.read()
