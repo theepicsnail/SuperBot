52ec457af3bebd0f8b1a8e1f07e5a3659c07fd28
UdderWeb Team <staff@udderweb.com>
2011-11-25 14:53:08 -0500

    Added magic 8-ball

--- Plugins/Chanutils.py
+++ Plugins/Chanutils.py
@@ -59,4 +59,8 @@ def on_PRIVMSG(bot,sender,msg):
     if parts[0][0]=="@":
         parts = msg[1].split(" ",1)
         bot.say(parts[0][1:],parts[1])
-
+    if parts[0]=="!8":
+        from random import choice
+        m = ["As I see it, yes","It is certain","It is decidedly so","Most likely","Outlook good","Signs point to yes","Without a doubt","Yes","Yes - definitely","You may rely on it","Reply hazy, try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+        c = choice(m)
+        bot.say(msg[0],c)
